0ec3f3c10095e5656a7c7f229942531b7f87cee8
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2025-04-22 00:40:09 -0300

    new module to compute fractal

---
+++ include/mandelbrot.h
@@ -0,0 +1,9 @@
+#ifndef __MANDELBROT_H_
+#define __MANDELBROT_H_
+
+#include <complex.h>
+#include <math.h>
+
+int mandelbrot(double real, double imag, int max_depth);
+
+#endif
---
+++ src/mandelbrot.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "mandelbrot.h"
+
+int mandelbrot(double real, double imag, int max_depth) {
+  double complex c = real + imag * I;
+  double complex z = 0 + 0 * I;
+  int iter = 0;
+  while (cabs(z) <= 2.0 && iter < max_depth) {
+    z = z * z + c;
+    iter++;
+  }
+  printf("real %f imag %f -> %d\n", real, imag, iter);
+  return iter;
+}
